0ed15152f3d6af46bddbee1a89ce3c8ba9829f15
kerrygao <1509699669@qq.com>
2016-02-29 16:20:12 +0800

    ad

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -2,7 +2,30 @@
 namespace Home\Controller;
 use Think\Controller;
 class BaseController extends Controller {
-    public function index(){
-    	$this->display();
+	/**
+	 * [_initialize 初始化函数]
+	 * @return [type] [description]
+	 */
+	public function _initialize () {
+
+	}
+
+	public function _empty() {
+        header("HTTP/1.0 404 Not Found");
+        $this->display('Public:404');
+    }
+
+ /**
+   * [resultMsg 公共信息返回]
+   * @param  [type] $status [返回状态,success或error]
+   * @param  [type] $msg    [返回消息]
+   * @param  string $data   [返回值]
+   * @return [type]         [json]
+   */
+    public function resultMsg($status, $msg, $data = '') {
+        $array['status'] = $status;
+        $array['message'] = $msg;
+        $array['data'] = $data;
+        $this->ajaxReturn($array, 'json');
     }
 }
